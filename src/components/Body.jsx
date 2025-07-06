6ee8d8a41ae7f713de20178909e7154051227666
alankarmundkar <alankarmundkar123@gmail.com>
2025-07-07 01:00:55 +0530

    unauthourized handled

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { Outlet, useNavigate } from "react-router-dom";
+import { Outlet, useNavigate, useLocation } from "react-router-dom";
 import Footer from "./Footer";
 import NavBar from "./NavBar";
 import axios from "../utils/axios";
@@ -9,37 +9,50 @@ import { addUser } from "../utils/userSlice";
 import { addToast } from "../utils/globalSlice";
 
 const Body = () => {
-
   const dispatch = useDispatch();
-  const navigate = useNavigate()
-  const userData = useSelector((state)=>state.user.userData)
+  const navigate = useNavigate();
+  const location = useLocation();
+  const userData = useSelector((state) => state.user.userData);
+
+  // Check if current route is public (login/signup)
+  const isPublicRoute = location.pathname === '/login' || location.pathname === '/signup';
 
   useEffect(() => {
     const fetchUser = async () => {
       try {
-        if(Object.keys(userData).length > 0 ){
-          return
+        // Don't fetch user data if we're on public routes
+        if (isPublicRoute) {
+          return;
         }
+
+        // Don't fetch if user data already exists
+        if (Object.keys(userData).length > 0) {
+          return;
+        }
+
         const { data } = await axios.get(endPoints.getProfile);
-        console.log(data,'dataa')
+        console.log(data, 'dataa');
         dispatch(addUser(data));
       } catch (error) {
         console.log(error, "fetchUser failed");
-        if(error.code === "ERR_NETWORK"){
-          console.log("error.code",error.code);
+        if (error.code === "ERR_NETWORK") {
+          console.log("error.code", error.code);
           dispatch(addToast({
             show: true,
             message: "Please check your internet connection and try again",
             type: "info",
           }));
         }
-        if(error.status == 401){
-          navigate('/login')
+        if (error.response && error.response.status === 401) {
+          // Only redirect if not already on login page
+          if (!isPublicRoute) {
+            navigate('/login');
+          }
         }
       }
     };
     fetchUser();
-  }, []);
+  }, [isPublicRoute, userData, dispatch, navigate]);
 
   return (
     <div>
--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,9 +1,35 @@
 import axios from "axios";
 import { BASE_URL } from "./constant";
+import { removeUser } from "./userSlice";
+import { removeFeed } from "./feedSlice";
+import { resetGlobal } from "./globalSlice";
+import { appStore } from "./appStore";
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
   withCredentials: true
-})
+});
 
-export default  axiosInstance
+// Response interceptor to handle 401 errors
+axiosInstance.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response && error.response.status === 401) {
+      // Clear all store data
+      appStore.dispatch(removeUser());
+      appStore.dispatch(removeFeed());
+      appStore.dispatch(resetGlobal());
+      
+      // Only redirect if not already on login/signup page
+      const currentPath = window.location.pathname;
+      if (currentPath !== '/login' && currentPath !== '/signup') {
+        window.location.href = '/login';
+      }
+    }
+    return Promise.reject(error);
+  }
+);
+
+export default axiosInstance;
